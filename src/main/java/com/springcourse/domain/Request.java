4bfd6807501f2cea78edd4e46aa939fa9dd852d8
D4GUR4SU <douglas_search@hotmail.com>
2024-01-23 08:25:39 -0300

    Mapping entities

--- src/main/java/com/springcourse/domain/Request.java
+++ src/main/java/com/springcourse/domain/Request.java
@@ -4,6 +4,17 @@ import java.util.Date;
 
 import com.springcourse.enums.RequestState;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +24,28 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "request")
 public class Request {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+
+	@Column(name = "creation_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date creationDate;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
 }
--- src/main/java/com/springcourse/domain/RequestStage.java
+++ src/main/java/com/springcourse/domain/RequestStage.java
@@ -4,6 +4,17 @@ import java.util.Date;
 
 import com.springcourse.enums.RequestState;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +24,30 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "request_stage")
 public class RequestStage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "realization_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date realizationDate;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "request_id", nullable = false)
+	private Request request;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 }
