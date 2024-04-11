ce1534951d08166a6308d9748596170743fa2d00
D4GUR4SU <douglas_search@hotmail.com>
2024-04-11 15:37:17 -0300

    Creating UserResource

---
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -0,0 +1,54 @@
+package com.springcourse.resource;
+
+import com.springcourse.domain.User;
+import com.springcourse.dto.UserLoginDto;
+import com.springcourse.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User createdUser = userService.save(user);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") Long id, @RequestBody User user) {
+        user.setId(id);
+        User updatedUser = userService.update(user);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable("id") Long id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listAll() {
+        List<User> users = userService.listAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody UserLoginDto userLoginDto) {
+        User loggedUser = userService.login(userLoginDto.getEmail(), userLoginDto.getPassword());
+
+        return ResponseEntity.ok(loggedUser);
+    }
+
+}
